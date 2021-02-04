5e29f9ca3431b71dade2a7715fb3373de0c6a6da
yo <yo@plopito.tk>
2021-02-04 16:22:22 +0100

    Support postfix-$instance/submission/smtpd[$PID] log lines

--- postfix-log-parser.go
+++ postfix-log-parser.go
@@ -10,7 +10,7 @@ const (
 	TimeFormatISO8601          = "2006-01-02T15:04:05.999999-07:00"
 	TimeRegexpFormat           = `([A-Za-z]{3}\s*[0-9]{1,2} [0-9]{2}:[0-9]{2}:[0-9]{2}|^\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+(?:[+-][0-2]\d:[0-5]\d|Z))`
 	HostRegexpFormat           = `([0-9A-Za-z\-\.]*)`
-	ProcessRegexpFormat        = `(postfix/[a-z]*\[[0-9]{1,5}\])?`
+	ProcessRegexpFormat        = `(postfix.*(\/[a-z]*)+\[[0-9]{1,5}\])?`
 	QueueIdRegexpFormat        = `([0-9A-Z]*)`
 	MessageDetailsRegexpFormat = `((?:client=(.+)\[(.+)\])?(?:message-id=<(.+)>)?(?:from=<(.+@.+)>)?(?:to=<(.+@.+)>.*status=([a-z]+))?.*)`
 	RegexpFormat               = TimeRegexpFormat + ` ` + HostRegexpFormat + ` ` + ProcessRegexpFormat + `: ` + QueueIdRegexpFormat + `(?:\: )?` + MessageDetailsRegexpFormat
@@ -59,14 +59,14 @@ func (p *PostfixLog) Parse(text []byte) (LogFormat, error) {
 		Time:           &t,
 		Hostname:       string(group[2]),
 		Process:        string(group[3]),
-		QueueId:        string(group[4]),
-		Messages:       string(group[5]),
-		ClientHostname: string(group[6]),
-		ClinetIp:       string(group[7]),
-		MessageId:      string(group[8]),
-		From:           string(group[9]),
-		To:             string(group[10]),
-		Status:         string(group[11]),
+		QueueId:        string(group[5]),
+		Messages:       string(group[6]),
+		ClientHostname: string(group[7]),
+		ClinetIp:       string(group[8]),
+		MessageId:      string(group[9]),
+		From:           string(group[10]),
+		To:             string(group[11]),
+		Status:         string(group[12]),
 	}
 
 	return logFormat, nil

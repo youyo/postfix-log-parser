46eee9e75f1ee2b22308a13f1323b66e12511459
yo <johan@nosd.in>
2022-07-23 13:20:54 +0200

    version bump to 1.4.2 b/c dumbitude is intense

--- postfix-log-parser/cmd/root.go
+++ postfix-log-parser/cmd/root.go
@@ -77,7 +77,7 @@ var (
 	File   os.File
 	Writer *bufio.Writer
 
-	Version = "1.4.1"
+	Version = "1.4.2"
 
 	BuildInfo = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "postfixlogparser_build_info",

64764600074259653008199c925fd2effbd61fdd
yo <johan@nosd.in>
2022-11-18 13:47:20 +0100

    Change version number

--- postfix-log-parser/cmd/root.go
+++ postfix-log-parser/cmd/root.go
@@ -77,7 +77,7 @@ var (
 	File   os.File
 	Writer *bufio.Writer
 
-	Version = "1.4.3"
+	Version = "1.4.4"
 
 	BuildInfo = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "postfixlogparser_build_info",
